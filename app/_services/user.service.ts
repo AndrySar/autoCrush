9d9968fd7661b4c8dfea07e5bc8b1c0092828a76
AndrySar <moisandry@gmail.com>
2017-04-05 00:30:40 +0300

    base architecture

---
+++ app/_services/user.service.ts
@@ -0,0 +1,49 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+
+import { User } from '../_models/index';
+
+@Injectable()
+export class UserService {
+    constructor(private http: Http) { }
+
+    getAll() {
+        return this.http.get('/api/users', this.jwt()).map((response: Response) => response.json());
+    }
+
+    getById(id: number) {
+        return this.http.get('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+    }
+
+    create(user: User) {
+        return this.http.post('http://localhost:3002/api/user', user, {headers: this.contentType()}).map((response: Response) => response.json());
+    }
+
+    update(user: User) {
+        return this.http.put('/api/users/' + user.id, user, this.jwt()).map((response: Response) => response.json());
+    }
+
+    delete(id: number) {
+        return this.http.delete('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+    }
+
+    // private helper methods
+
+    private jwt() {
+        // create authorization header with jwt token
+        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        if (currentUser && currentUser.token) {
+            let headers = new Headers();
+            headers.append('x-access-token', currentUser.token);
+            headers.append('x-key', currentUser.username);
+            headers.append('Content-Type', 'application/json; charset=UTF-8');
+            return headers;
+        }
+    }
+
+    private contentType(){
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json; charset=UTF-8');
+        return headers;
+    }
+}
